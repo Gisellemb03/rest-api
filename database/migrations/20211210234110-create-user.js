fad972890dafe085b9eea12a2b39b66fa907fa7d
Gisellemb03 <gisellemendozab03@gmail.com>
2021-12-12 10:50:06 -0600

    actualizar

--- database/migrations/20211210234110-create-user.js
+++ database/migrations/20211210234110-create-user.js
@@ -20,6 +20,9 @@ module.exports = {
       comments: {
         type: Sequelize.TEXT
       },
+      imagen: {
+        type: Sequelize.TEXT
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
